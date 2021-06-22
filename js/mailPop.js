34cfc056815757091fa94b533cf8e8d9b21cd840
cti1650 <cti1650@gmail.com>
2021-06-22 20:04:19 +0900

    test

--- js/mailPop.js
+++ js/mailPop.js
@@ -1,16 +1,38 @@
-function mailPop(opt={}){
+function mailPop(opt = {}) {
   let def_data = {
-    'subject':'',
-    'body':'',
-    'to':[''],
-    'replace':{},
+    subject: '',
+    body: '',
+    to: [''],
+    replace: {},
+    mode: 'gmail',
   };
-  let data = {...def_data,...opt};
-  if(data.replace){
-    Object.keys(data.replace).forEach(key => {
-      data.subject = data.subject.replace('{{' + key + '}}',data.replace[key]);
-      data.body = data.body.replace('{{' + key + '}}',data.replace[key]);
+  let data = { ...def_data, ...opt };
+  if (data.replace) {
+    Object.keys(data.replace).forEach((key) => {
+      data.subject = data.subject.replace('{{' + key + '}}', data.replace[key]);
+      data.body = data.body.replace('{{' + key + '}}', data.replace[key]);
     });
   }
-  window.open("https://outlook.office.com/mail/deeplink/compose?subject=" + encodeURI(data.subject) + "&body=" + encodeURI(data.body) + "&to=" + encodeURI(data.to.join(';')));
-};
+  switch (data.mode) {
+    case 'ms':
+    case 'outlook':
+      window.open(
+        'https://outlook.office.com/mail/deeplink/compose?subject=' +
+          encodeURI(data.subject) +
+          '&body=' +
+          encodeURI(data.body) +
+          '&to=' +
+          encodeURI(data.to.join(';'))
+      );
+      break;
+    default:
+      window.open(
+        'https://mail.google.com/mail/?view=cm&su=' +
+          encodeURI(data.subject) +
+          '&body=' +
+          encodeURI(data.body) +
+          '&to=' +
+          encodeURI(data.to.join(';'))
+      );
+  }
+}
