611a07fe4ba711cf129e8b1375c1fec37e1e72b5
cti1650 <cti1650@gmail.com>
2021-06-22 15:56:44 +0900

    Update mailPop.js

--- js/mailPop.js
+++ js/mailPop.js
@@ -6,5 +6,11 @@ function mailPop(opt={}){
     'replace':{},
   };
   let data = {...def_data,...opt};
+  if(data.replace){
+    Object.keys(data.replace).forEach(key => {
+      data.subject = data.subject.replace('{{' + key + '}}',data.replace[key]);
+      data.body = data.body.replace('{{' + key + '}}',data.replace[key]);
+    });
+  }
   window.open("https://outlook.office.com/mail/deeplink/compose?subject=" + encodeURI(data.subject) + "&body=" + encodeURI(data.body) + "&to=" + encodeURI(data.to.join(';')));
 };
